fc84196764a359503268987753d588db6d73fabd
lidening <282093008@qq.com>
2026-04-15 14:03:44 +0800

    修改为deepseek模型
    修改为deepseek模型

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 聊天机器人")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+    "这是基于deepseek大模型的简单聊天机器人"
+    "需要输入可用的deepseek API key"
+    )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = st.text_input("deepseek API Key", type="password")
+chat_model="deepseek-chat"
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key, base_url="https://api.deepseek.com")
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -41,7 +41,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model=chat_model,
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
